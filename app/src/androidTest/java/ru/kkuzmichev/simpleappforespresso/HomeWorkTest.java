0841d148890be8f0327efe73ed425bb6f6a96b76
Vladislav0306 <vlad.ezhkov.96@mail.ru>
2022-10-12 13:19:49 +0300

    Initial commit

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
@@ -1,11 +1,25 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.content.Intent;
+import android.widget.ActionMenuView;
+
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -15,19 +29,27 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class HomeWorkTest {
+
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule =
-            new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testName() {
-        ViewInteraction mainText = onView(
-                withId(R.id.text_home)
+    public void testOpenRightMenu() {
+        ViewInteraction element = onView(
+                withContentDescription("More options")
         );
-        mainText.check(
-                matches(
-                        withText("This is home fragment")
+        element.perform(click());
+        ViewInteraction elementId = onView(
+                withId(R.id.title)
+        );
+        elementId.check(matches
+                (withText("Settings")
                 )
         );
+        Intents.init();
+        elementId.perform(click());
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
     }
 }
