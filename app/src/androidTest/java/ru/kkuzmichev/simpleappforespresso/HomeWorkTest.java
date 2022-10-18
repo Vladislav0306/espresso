5185ea4f5f21b7dac65133c876fc8f88ae04e866
Vladislav0306 <vlad.ezhkov.96@mail.ru>
2022-10-18 20:36:47 +0300

    Initial commit

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
@@ -6,23 +6,18 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-import static org.hamcrest.core.AllOf.allOf;
 
 import android.content.Intent;
-import android.widget.ActionMenuView;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,19 +34,18 @@ public class HomeWorkTest {
 
     @Test
     public void testOpenRightMenu() {
-        ViewInteraction element = onView(
-                withContentDescription("More options")
-        );
-        element.perform(click());
-        ViewInteraction elementId = onView(
-                withId(R.id.title)
-        );
-        elementId.check(matches
-                (withText("Settings")
-                )
-        );
+        ViewInteraction imageView = onView(
+                Matchers.allOf(
+                        withParent(withParent(withId(R.id.toolbar))),
+                        isDisplayed()));
+        imageView.perform(click());
+        ViewInteraction textView = onView(
+                Matchers.allOf(withId(androidx.appcompat.R.id.title),
+                        withParent(withParent(withId(androidx.appcompat.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withParent(withParent(withId(androidx.appcompat.R.id.content)))));
         Intents.init();
-        elementId.perform(click());
+        textView.perform(click());
         intended(hasData("https://google.com"));
         intended(hasAction(Intent.ACTION_VIEW));
         Intents.release();
