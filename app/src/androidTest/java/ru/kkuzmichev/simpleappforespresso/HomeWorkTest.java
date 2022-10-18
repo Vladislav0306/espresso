d907a71bfb5ec90c79060269076d337be3b48127
Vladislav0306 <vlad.ezhkov.96@mail.ru>
2022-10-18 08:42:04 +0300

    Initial commit

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
@@ -3,12 +3,13 @@ package ru.kkuzmichev.simpleappforespresso;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.hamcrest.CoreMatchers.allOf;
 
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -41,11 +42,10 @@ public class HomeWorkTest {
 
     @Test
     public void testOpenGallery() {
-        ViewInteraction element = onView(
-                withContentDescription("Open navigation drawer")
-        );
-        element.perform(click());
-        ViewInteraction gallery = onView(withText("Gallery"));
+        ViewInteraction menu = onView(isAssignableFrom(AppCompatImageButton.class));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
         gallery.perform(click());
         ViewInteraction itemSeven = onView(allOf(withId(R.id.item_number), withText("7")));
         itemSeven.check(matches(withText("7")));
